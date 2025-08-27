7471dd3b398b2a7a3307c33480f0b1d1ec4a0979
RodriBau <rodri.bp.gg@gmail.com>
2025-08-26 21:35:29 -0400

    prueba brevo con libreria #6

--- services/emailService.js
+++ services/emailService.js
@@ -1,5 +1,6 @@
 const SibApiV3Sdk = require("sib-api-v3-sdk");
 const defaultClient = SibApiV3Sdk.ApiClient.instance;
+
 const apiKey = defaultClient.authentications["api-key"];
 apiKey.apiKey = process.env.BREVO_API_KEY;
 
@@ -8,9 +9,12 @@ const apiInstance = new SibApiV3Sdk.TransactionalEmailsApi();
 async function sendEmail({ to, subject, text, html, attachments = [] }) {
   try {
     const sendSmtpEmail = new SibApiV3Sdk.SendSmtpEmail({
-      sender: { name: "SIREDE", email: "pablo.crj.mss@gmail.com" },
+      sender: {
+        name: "SIREDE",
+        email: process.env.BREVO_FROM, 
+      },
       to: [{ email: to }],
-      subject: subject,
+      subject,
       textContent: text,
       htmlContent: html,
       attachment: attachments.map((attachment) => ({
@@ -20,10 +24,13 @@ async function sendEmail({ to, subject, text, html, attachments = [] }) {
     });
 
     const data = await apiInstance.sendTransacEmail(sendSmtpEmail);
-    console.log("Correo enviado con Brevo. Message ID: ", data.messageId);
+    console.log("Correo enviado con Brevo. Message ID:", data.messageId);
     return data;
   } catch (error) {
-    console.error("Error al enviar correo con Brevo:", error);
+    console.error(
+      "Error al enviar correo con Brevo:",
+      error.response?.body || error
+    );
     throw error;
   }
 }
